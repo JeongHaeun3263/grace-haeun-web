7d7ed789910ce009987f4db63967f3d6bdf431a9
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2022-02-22 20:10:10 -0500

    Modify About Page

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ const About = () => {
 				<div className={styles.profilebox}>
 					<Image
 						className={styles.profile}
-						src='/profile.jpeg'
+						src='/profile.png'
 						width={300}
 						height={300}
 						alt='Profile'
@@ -26,14 +26,29 @@ const About = () => {
 					<h1>Hi there,</h1>
 					<h2>My name is Grace.</h2>
 					<p>
-						I am a Front End Web Developer. I have a passion for web development
-						and I especially enjoy working with front end technologies.
+						I’m a front-end developer. I have a passion for web & software
+						development and I especially enjoy working with front-end
+						technologies.
 					</p>
-					<p>I like interacting with people and inspiring each other.</p>
 					<p>
-						I love exploring new places, breathtaking nature, and amazing food
-						around the world. I have traveled to 7+ countries, 20+ cities!
+						Each day, I try to learn something new and expand my knowledge of
+						programming. My goal is to rub elbows with the big boys, and I come
+						close to getting there every time I master a pogromming skill.
 					</p>
+					<p>
+						I am also a people person, and I very much enjoy interacting and
+						collaborating with others to produce exciting new things.
+					</p>
+					<p>
+						In my free time, I love finding new places to eat and drink; I
+						equally relish in exploring the nooks and crannies of Toronto, but I
+						especially indulge in discovering the natural beauty of Ontrio.
+					</p>
+					<p>
+						And even though I’ve been places, I often fantasize about seeing the
+						world.
+					</p>
+					<p className={styles.signature}>Grace</p>
 				</div>
 			</section>
 		</Fragment>
